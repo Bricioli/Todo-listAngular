181fbdb8c774cbee773d9c3db6bc093b711d79b7
Jared Ricioli <jared.ricioli@live.com>
2022-03-08 15:22:26 -0300

    Implementada a lista de chamados

--- src/app/tarefas/listar-tarefa/listar-tarefa.component.ts
+++ src/app/tarefas/listar-tarefa/listar-tarefa.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TarefaService, Tarefa } from '../shared';
+
 
 @Component({
   selector: 'app-listar-tarefa',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarTarefaComponent implements OnInit {
 
-  constructor() { }
+  tarefas: Tarefa [];
+  constructor(private tarefaService: TarefaService) {
 
-  ngOnInit(): void {
   }
 
+  ngOnInit(): void {
+    this.tarefas = this.listarTodos();
+    this.tarefas = [
+      new Tarefa(1, "Testetando", false),
+      new Tarefa(2, "oloco bixo", true)
+    ]
+  }
+
+  listarTodos(): Tarefa[] {
+    return this.tarefaService.listarTodos();
+  }
+
+  remover($event: any, tarefa: Tarefa): void {
+    if (confirm('Deseja remover a tarefa "' + tarefa.nome + '"?'))
+      this.tarefaService.remover(tarefa.id);
+      this.tarefas = this.tarefaService.listarTodos();
+  }
 }
